ef06ad4018cb8a0bed74b90c03244a931e6f460e
Redy <redy.ru@gmail.com>
2023-09-08 00:58:40 +0800

    add toolbar, ready to replace icons

--- src/main.gd
+++ src/main.gd
@@ -1,8 +1,12 @@
 extends Control
 
 
-
-
 func _on_navbar_navigation_to(item_id, item_data):
-	print(item_id, '--->', item_data)
-	
+
+	match item_id:
+		Navbar.NEW_FILE:
+			pass
+		Navbar.OPEN_FILE:
+			pass
+			
+
