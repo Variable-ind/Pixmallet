6cea4e66f7addd5a36e1d2f033254e3e6af60b71
Redy <redy.ru@gmail.com>
2023-09-12 18:23:18 +0800

    xxxx

--- src/widget/ValueSlider.gd
+++ src/widget/ValueSlider.gd
@@ -1,8 +1,8 @@
 @tool
 class_name ValueSlider
 extends TextureProgressBar
-## Custom node that combines the behavior of a Slider and a SpinBox.
-## Initial version made by MrTriPie, has been modified by Overloaded.
+# Custom node that combines the behavior of a Slider and a SpinBox.
+# Initial version made by MrTriPie, has been modified by Overloaded.
 
 enum { NORMAL, HELD, SLIDING, TYPING }
 
@@ -15,16 +15,16 @@ enum { NORMAL, HELD, SLIDING, TYPING }
 	set(v):
 		suffix = v
 		_reset_display()
-## Size of additional snapping (applied in addition to Range's step).
-## This should always be larger than step.
+# Size of additional snapping (applied in addition to Range's step).
+# This should always be larger than step.
 @export var snap_step := 1.0
-## If snap_by_default is true, snapping is enabled when Control is NOT held (used for sliding in
-## larger steps by default, and smaller steps when holding Control).
-## If false, snapping is enabled when Control IS held (used for sliding in smaller steps by
-## default, and larger steps when holding Control).
+# If snap_by_default is true, snapping is enabled when Control is NOT held (used for sliding in
+# larger steps by default, and smaller steps when holding Control).
+# If false, snapping is enabled when Control IS held (used for sliding in smaller steps by
+# default, and larger steps when holding Control).
 @export var snap_by_default := false
-## If show_progress is true it will show the colored progress bar, good for values with a specific
-## range. False will hide it, which is good for values that can be any number.
+# If show_progress is true it will show the colored progress bar, good for values with a specific
+# range. False will hide it, which is good for values that can be any number.
 @export var show_progress := true
 @export var show_arrows := true:
 	set(v):
@@ -34,11 +34,17 @@ enum { NORMAL, HELD, SLIDING, TYPING }
 		_value_up_button.visible = v
 		_value_down_button.visible = v
 @export var echo_arrow_time := 0.075
-@export var global_increment_action := ""  ## Global shortcut to increment
-@export var global_decrement_action := ""  ## Global shortcut to decrement
+@export var global_increment_action := ""  # Global shortcut to increment
+@export var global_decrement_action := ""  # Global shortcut to decrement
+
+@export var modulate_icon_color :Color = Color.WHITE:  # change icon color
+	set(color): 
+		modulate_icon_color = color
+		_value_up_button.modulate = color
+		_value_down_button.modulate = color
 
 var state := NORMAL
-var arrow_is_held := 0  ## Used for arrow button echo behavior. Is 1 for ValueUp, -1 for ValueDown.
+var arrow_is_held := 0  # Used for arrow button echo behavior. Is 1 ValueUp, -1 ValueDown.
 
 var _line_edit := LineEdit.new()
 var _value_up_button := TextureButton.new()
@@ -57,11 +63,9 @@ func _init() -> void:
 
 func _ready() -> void:
 	value_changed.connect(_on_value_changed)
-	set_process_input(!global_increment_action.is_empty() and !global_decrement_action.is_empty())
+	set_process_input(!global_increment_action.is_empty() and \
+					  !global_decrement_action.is_empty())
 	_reset_display(true)
-	if not Engine.is_editor_hint():  # Pixelorama specific code
-		_value_up_button.modulate = Global.modulate_icon_color
-		_value_down_button.modulate = Global.modulate_icon_color
 	_setup_nodes()
 
 
@@ -91,8 +95,8 @@ func _gui_input(event: InputEvent) -> void:
 	if not editable:
 		return
 	if state == NORMAL:
-		if event is InputEventMouseButton and event.pressed:
-			if event.button_index == MOUSE_BUTTON_LEFT:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 				state = HELD
 				set_meta("mouse_start_position", get_local_mouse_position())
 			elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
@@ -106,7 +110,8 @@ func _gui_input(event: InputEvent) -> void:
 				else:
 					value -= snap_step if event.ctrl_pressed else step
 	elif state == HELD:
-		if event.is_action_released("left_mouse"):
+		if (event is InputEventMouseButton and event.pressed and \
+			event.button_index == MOUSE_BUTTON_LEFT):
 			state = TYPING
 			_line_edit.text = str(value)
 			_line_edit.editable = true
@@ -122,20 +127,23 @@ func _gui_input(event: InputEvent) -> void:
 				set_meta("start_ratio", ratio)
 				set_meta("start_value", value)
 	elif state == SLIDING:
-		if event.is_action_released("left_mouse"):
+		if (event is InputEventMouseButton and event.pressed == false and \
+			event.button_index == MOUSE_BUTTON_LEFT):
 			state = NORMAL
 			remove_meta("mouse_start_position")
 			remove_meta("start_ratio")
 			remove_meta("start_value")
 			remove_meta("shift_pressed")
 		if event is InputEventMouseMotion:
-			# When pressing/releasing Shift, reset starting values to prevent slider jumping around
+			# When pressing/releasing Shift, 
+			# reset starting values to prevent slider jumping around
 			if get_meta("shift_pressed") != event.shift_pressed:
 				set_meta("mouse_start_position", get_local_mouse_position())
 				set_meta("start_ratio", ratio)
 				set_meta("start_value", value)
 				set_meta("shift_pressed", event.shift_pressed)
-			var x_delta: float = get_local_mouse_position().x - get_meta("mouse_start_position").x
+			var x_delta: float = get_local_mouse_position().x - \
+								 get_meta("mouse_start_position").x
 			# Slow down to allow for more precision
 			if event.shift_pressed:
 				x_delta *= 0.1
@@ -152,7 +160,7 @@ func _gui_input(event: InputEvent) -> void:
 					value = roundf(value / snap_step) * snap_step
 
 
-func _setup_nodes() -> void:  ## Only called once on _ready()
+func _setup_nodes() -> void:  # Only called once on _ready()
 	focus_mode = Control.FOCUS_ALL
 	_line_edit.alignment = HORIZONTAL_ALIGNMENT_CENTER
 	_line_edit.anchor_right = 1
@@ -168,6 +176,7 @@ func _setup_nodes() -> void:  ## Only called once on _ready()
 	var value_up_texture_size := Vector2.ONE
 	if is_instance_valid(_value_up_button.texture_normal):
 		value_up_texture_size = _value_up_button.texture_normal.get_size()
+		
 	_value_up_button.scale.y = -1
 	_value_up_button.anchor_left = 1
 	_value_up_button.anchor_right = 1
@@ -184,6 +193,7 @@ func _setup_nodes() -> void:  ## Only called once on _ready()
 	var value_down_texture_size := Vector2.ONE
 	if is_instance_valid(_value_down_button.texture_normal):
 		value_down_texture_size = _value_down_button.texture_normal.get_size()
+	
 	_value_down_button.anchor_left = 1
 	_value_down_button.anchor_top = 1
 	_value_down_button.anchor_right = 1
@@ -213,13 +223,13 @@ func _on_value_changed(_value: float) -> void:
 	_reset_display()
 
 
-## When pressing enter, release focus, which will call _confirm_text on focus_exited signal
+# When pressing enter, release focus, which will call _confirm_text on focus_exited signal
 func _on_LineEdit_text_entered(_new_text: String) -> void:
 	_line_edit.release_focus()
 
 
-## Called on LineEdit's focus_exited signal
-## If confirm is false it will cancel setting value
+# Called on LineEdit's focus_exited signal
+# If confirm is false it will cancel setting value
 func _confirm_text(confirm := true) -> void:
 	if state != TYPING:
 		return
@@ -258,12 +268,14 @@ func _reset_display(theme_has_changed := false) -> void:
 		if editable:
 			_line_edit.add_theme_color_override("font_color_uneditable", line_edit_color)
 		else:
-			_line_edit.add_theme_color_override("font_color_uneditable", line_edit_disabled_col)
+			_line_edit.add_theme_color_override("font_color_uneditable",
+												 line_edit_disabled_col)
 		tint_under = get_theme_color("under_color", "ValueSlider")
 		if show_progress:
 			tint_progress = get_theme_color("progress_color", "ValueSlider")
 		else:
 			tint_progress = Color.TRANSPARENT
+			
 	_line_edit.text = str(tr(prefix), " ", value, " ", tr(suffix)).strip_edges()
 
 
@@ -283,7 +295,7 @@ func _on_Value_button_up() -> void:
 	_timer.stop()
 
 
-## Echo behavior. If the user keeps pressing the button, the value keeps changing.
+# Echo behavior. If the user keeps pressing the button, the value keeps changing.
 func _on_Timer_timeout() -> void:
 	if arrow_is_held == 0:
 		_timer.stop()
