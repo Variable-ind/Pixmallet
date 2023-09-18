f24351df087fc448766f0146a395195c7582d8d5
Redy <redy.ru@gmail.com>
2023-09-18 23:50:40 +0800

    xxx

--- src/adjustment/AdjustmentTool.gd
+++ src/adjustment/AdjustmentTool.gd
@@ -30,21 +30,7 @@ func _ready():
 	for	btn in get_children():
 		if btn is Button:
 			btn.pressed.connect(_on_pressed.bind(btn))
-			btn.mouse_entered.connect(_on_mouseover.bind(btn))
-			btn.mouse_exited.connect(_on_mouseout.bind(btn))
-			btn.self_modulate = Color.WEB_GRAY
 
 
 func _on_pressed(btn):
 	active_adjust_tool.emit(ADJUST_ID_MAP.get(btn.name, -1))
-	
-	
-func _on_mouseover(btn):
-	btn.self_modulate = Color.WHITE
-	
-	
-func _on_mouseout(btn):
-	if btn.toggle_mode and btn.button_pressed:
-		btn.self_modulate = Color.WHITE
-	else:
-		btn.self_modulate = Color.WEB_GRAY
