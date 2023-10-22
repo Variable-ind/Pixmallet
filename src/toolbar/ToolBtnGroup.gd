2a0cf11aa5406dd625efce1605cf61ca93ac3141
Redy <redy.ru@gmail.com>
2023-10-22 21:25:22 +0800

    xxxx

--- src/toolbar/ToolBtnGroup.gd
+++ src/toolbar/ToolBtnGroup.gd
@@ -16,8 +16,8 @@ func _ready():
 	long_press_timer.one_shot = true
 	long_press_timer.wait_time = LONG_PRESS_DELAY
 	long_press_timer.timeout.connect(show_popup)
-	
-	gui_input.connect(_on_gui_input)
+	button_down.connect(_on_button_down)
+	button_up.connect(_on_button_up)
 	
 	var erase_list: Array = []
 	for btn in group_btns:
@@ -68,22 +68,14 @@ func show_popup():
 	popup.show()
 
 
-func _on_gui_input(event):
-	if event is InputEventMouseButton:
-		if event.pressed and long_press_timer.is_stopped():
-			long_press_timer.start()
-		elif not long_press_timer.is_stopped():
-			long_press_timer.stop()
+func _on_button_down():
+	if long_press_timer.is_stopped():
+		long_press_timer.start()
 
 
-#func _on_button_down():
-#	if long_press_timer.is_stopped():
-#		long_press_timer.start()
-#
-#
-#func _on_button_up():
-#	if not long_press_timer.is_stopped():
-#		long_press_timer.stop()
+func _on_button_up():
+	if not long_press_timer.is_stopped():
+		long_press_timer.stop()
 
 
 func _on_select_extend_btn(btn):
@@ -92,4 +84,5 @@ func _on_select_extend_btn(btn):
 	await get_tree().create_timer(0.1).timeout
 	for b in group_btns:
 		b.button_pressed = false
+	button_pressed = true
 	pressed.emit()
