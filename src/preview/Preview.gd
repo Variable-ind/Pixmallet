0666d87d3cdd9bbd2f81ef79a5b70283a6c1c31f
Redy <redy.ru@gmail.com>
2023-10-30 01:09:55 +0800

    preview seems done

--- src/preview/Preview.gd
+++ src/preview/Preview.gd
@@ -3,7 +3,9 @@ class_name Preview extends Panel
 
 @onready var preview_canvas := %PreviewCanvas
 @onready var preview_camera := %PreviewCamera
+@onready var preview_viewport := %PreviewViewport
 @onready var preview_trans_checker := %PreviewTransChecker
+
 
 @onready var btn_play := %BtnPlay
 @onready var btn_play_reverse := %BtnPlayReverse
@@ -17,13 +19,21 @@ func _ready():
 	btn_play_reverse.toggled.connect(_on_play_reverse_toggled)
 	btn_zoom_out.pressed.connect(_on_zoom_out)
 	btn_zoom_in.pressed.connect(_on_zoom_in)
+
+	preview_viewport.size_changed.connect(_on_viewport_size_changed)
+	preview_camera.zoom_changed.connect(_on_zoom_changed)
+	
+	zoom_slider.min_value = preview_camera.zoom_level_min
+	zoom_slider.max_value = preview_camera.zoom_level_max
+	zoom_slider.step = preview_camera.zoom_step
 	zoom_slider.value_changed.connect(_on_zoom_slid)
 	
 
 func load_project(proj):
 	preview_canvas.attach_project(proj)
-	preview_camera.canvas_size = proj.size
 	preview_trans_checker.update_bounds(proj.size)
+	preview_camera.canvas_size = proj.size
+	preview_camera.viewport_size = preview_viewport.size
 
 
 func _on_play_toggled(btn_pressed):
@@ -53,5 +63,15 @@ func _on_zoom_in():
 	
 
 func _on_zoom_slid(value):
-	print(value)
 	preview_camera.zoom_to(value)
+
+
+func _on_viewport_size_changed():
+	preview_camera.viewport_size = preview_viewport.size
+
+
+func _on_zoom_changed(level):
+	zoom_slider.value = level
+	btn_zoom_in.disabled = level >= preview_camera.zoom_level_max
+	btn_zoom_out.disabled = level <= preview_camera.zoom_level_min
+
--- src/preview/PreviewCamera.gd
+++ src/preview/PreviewCamera.gd
@@ -1,46 +1,70 @@
 class_name PreviewCamera extends Camera2D
 
+signal zoom_changed(level)
+
 var canvas_size := Vector2i.ZERO:
 	set(val):
 		canvas_size = val
-		offset = canvas_size * 0.5
+		set_zoom_fit()
+var viewport_size := Vector2i.ZERO:
+	set(val):
+		viewport_size = val
+		set_zoom_fit()
 
-var zoom_level_max := 10.0
-var zoom_level_min := 1.0
-var zoom_level := 1.0
-var zoom_step := 1.0
+var zoom_ratio_max := 1.0
+var zoom_ratio_min := 0.1
+var zoom_ratio_step := 0.1
+
+var zoom_level_max := 10
+var zoom_level_min := 1
+var zoom_level := 1
+var zoom_step := 1
 
 var zoom_center_point := Vector2.ZERO
+var btn_pressed := false
 
 
-func zoom_to(val:float):
+func zoom_to(level:int):
+	zoom_level = level
+	var zoom_ratio = zoom_ratio_min + zoom_ratio_step * (zoom_level-1)
+	zoom_ratio = minf(zoom_ratio, zoom_ratio_max)
+	zoom = Vector2.ONE * zoom_ratio
 	zoom_center_point = canvas_size * 0.5
-	zoom_level = clamp(val, zoom_level_min, zoom_level_max)
-	zoom = Vector2.ONE * zoom_level
+	offset = zoom_center_point
+	zoom_changed.emit(zoom_level)
 
 
 func zoom_in():
-	zoom_center_point = canvas_size * 0.5
-	zoom_level = min(zoom_level + zoom_step, zoom_level_max)
-	zoom = Vector2.ONE * zoom_level
+	if zoom_level < zoom_level_max:
+		zoom_to(zoom_level + 1)
 
 
 func zoom_out():
-	zoom_center_point = canvas_size * 0.5
-	zoom_level = max(zoom_level - zoom_step, zoom_level_min)
-	zoom = Vector2.ONE * zoom_level
+	if zoom_level > zoom_level_min:
+		zoom_to(zoom_level - 1)
 
 
-func zoom_100():
-	zoom = Vector2.ONE
-	zoom_center_point = canvas_size * 0.5
-	offset = zoom_center_point
-	
-
-func fit_to_frame():
-	offset = canvas_size * 0.5
+func set_zoom_fit():
+	if canvas_size.x < 0 or canvas_size.y < 0 or viewport_size == Vector2i.ZERO:
+		return
 	var h_ratio = viewport_size.x / float(canvas_size.x)
 	var v_ratio = viewport_size.y / float(canvas_size.y)
-	var ratio = minf(h_ratio, v_ratio)
-	ratio = clampf(ratio, 0.1, ratio)
-	zoom = Vector2(ratio, ratio)
+	zoom_ratio_min = minf(h_ratio, v_ratio)
+	zoom_ratio_step = (zoom_ratio_max - zoom_ratio_min) / zoom_level_max
+	zoom_level = zoom_level_min
+	zoom_to(zoom_level)
+
+
+func _input(event: InputEvent):
+	if event is InputEventPanGesture and OS.get_name() != "Android":
+		# Pan Gesture on a laptop touchpad
+		offset = offset + event.delta * 7.0 / zoom
+		offset = offset.clamp(Vector2i.ZERO, canvas_size)
+	
+	if event is InputEventMouseButton:
+		btn_pressed = event.pressed
+		
+	if event is InputEventMouseMotion and btn_pressed:
+		offset = offset - event.relative / zoom
+		offset = offset.clamp(Vector2i.ZERO, canvas_size)
+		
