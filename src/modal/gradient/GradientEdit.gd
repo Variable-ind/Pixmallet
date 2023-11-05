a787dd642b332959b5ac1668c7de03b86f37a72d
Redy <redy.ru@gmail.com>
2023-11-06 06:53:26 +0800

    xxx

--- src/modal/gradient/GradientEdit.gd
+++ src/modal/gradient/GradientEdit.gd
@@ -4,7 +4,7 @@ signal updated(gradient, cc)
 
 var continuous_change := true
 var active_cursor: GradientCursor  
-## Showing a color picker popup to change a cursor's color
+# showing a color picker popup to change a cursor's color
 
 @onready var x_offset: float = size.x - GradientCursor.WIDTH
 @onready var texture_rect := $TextureRect
@@ -16,14 +16,14 @@ var active_cursor: GradientCursor
 
 
 func _ready():
-	create_cursor()
+	place_cursors()
 
 
-func create_cursor():
+func place_cursors():
 	for cur in texture_rect.get_children():
 		if cur is GradientCursor:
 			texture_rect.remove_child(cur)
-			cur.queue_free()
+			cur.remove()
 	for i in gradient.get_point_count():
 		var p: float = gradient.get_offset(i)
 		add_cursor(p * x_offset, gradient.get_color(i))
@@ -55,75 +55,42 @@ func add_cursor(x: float, color: Color):
 	cursor.color = color
 
 
-func select_color(cursor: GradientCursor, pos: Vector2):
+func select_cursor(cursor: GradientCursor, pos: Vector2):
 	active_cursor = cursor
 	color_picker.color = cursor.color
 	if pos.x > global_position.x + (size.x / 2.0):
 		pos.x = global_position.x + size.x
 	else:
 		pos.x = global_position.x - $Popup.size.x
-	$Popup.position = pos
-	$Popup.popup()
-
-
-func get_sorted_cursors() -> Array:
-	var array: Array[GradientCursor] = []
-	for c in texture_rect.get_children():
-		if c is GradientCursor:
-			array.append(c)
-	array.sort_custom(
-		func(a: GradientCursor, b: GradientCursor): return a.get_position() < b.get_position()
-	)
-	return array
+	color_picker_popup.position = pos
+	color_picker_popup.popup()
 
 
 func get_gradient_color(x: float) -> Color:
 	return gradient.sample(x / x_offset)
 
 
-func _on_ColorPicker_color_changed(color: Color) -> void:
-	active_cursor.set_color(color)
+func _on_color_changed(color: Color) -> void:
+	active_cursor.color = color
 
 
-func _on_GradientEdit_resized() -> void:
+func _on_resized() -> void:
 	if not gradient:
 		return
 	x_offset = size.x - GradientCursor.WIDTH
-	_create_cursors()
+	place_cursors()
 
 
-func _on_InterpolationOptionButton_item_selected(index: Gradient.InterpolationMode) -> void:
+func set_interpolation_mode(index: Gradient.InterpolationMode):
 	gradient.interpolation_mode = index
-
-
-func _on_DivideButton_pressed() -> void:
-	divide_dialog.popup_centered()
-
-
-func _on_DivideConfirmationDialog_confirmed() -> void:
-	var add_point_to_end := add_point_end_check_box.button_pressed
-	var parts := number_of_parts_spin_box.value
-	var colors: PackedColorArray = []
-	var end_point := 1 if add_point_to_end else 0
-	parts -= end_point
-
-	if not add_point_to_end:
-		# Move the final color one part behind, useful for it to be in constant interpolation
-		gradient.add_point((parts - 1) / parts, gradient.sample(1))
-	for i in parts + end_point:
-		colors.append(gradient.sample(i / parts))
-	gradient.offsets = []
-	for i in parts + end_point:
-		gradient.add_point(i / parts, colors[i])
-	_create_cursors()
-	updated.emit(gradient, continuous_change)
 
 
 class GradientCursor extends Control:
 	
-	signal double_clicked(gcursor)
-	signal right_clicked(gcursor)
+	signal selected(gcursor)
+	signal removed(gcursor)
 	signal pressed(gcursor)
+	signal released(gcursor)
 
 	const WIDTH := 10
 	
@@ -153,23 +120,25 @@ class GradientCursor extends Control:
 		else:
 			draw_polyline(polygon, Color(1.0, 1.0, 1.0))
 
-	func _gui_input(ev: InputEvent) -> void:
+	func _gui_input(ev: InputEvent):
 		if ev is InputEventMouseButton:
 			if ev.button_index == MOUSE_BUTTON_LEFT:
 				if ev.double_click:
-					double_clicked.emit(self)
+					selected.emit(self)
 				elif ev.pressed:
 					pressed.emit(self)
 					is_sliding = true
 				else:
+					released.emit(self)
 					is_sliding = false
 			elif ev.button_index == MOUSE_BUTTON_RIGHT:
-				right_clicked.emit(self)
+				removed.emit(self)
+				remove()
 
 	func remove():
 		queue_free()
 
-	func _can_drop_data(_position, data) -> bool:
+	func _can_drop_data(_position, data):
 		return data is Color
 
 	func _drop_data(_position, data):
